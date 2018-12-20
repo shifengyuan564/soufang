92fdea8b34678b0c1a9992c86094214e40693de2
shifengyuan564 <shifengyuan564@126.com>
2018-12-20 22:42:15 +0800

    3-4

---
+++ src/main/java/com/imooc/soufang/controller/HomeController.java
@@ -0,0 +1,44 @@
+package com.imooc.soufang.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by 瓦力.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("name", "慕课");
+        return "index";
+    }
+
+    @GetMapping("/404")
+    public String notFoundPage() {
+        return "404";
+    }
+
+    @GetMapping("/403")
+    public String accessError() {
+        return "403";
+    }
+
+    @GetMapping("/500")
+    public String internalError() {
+        return "500";
+    }
+
+    @GetMapping("/logout/page")
+    public String logoutPage() {
+        return "logout";
+    }
+
+
+}
